dbd988bde851eef54d862d485123c92cc168993c
Mi Seadi <100794047+michele-seadi@users.noreply.github.com>
2024-05-09 19:01:05 +0000

    altereção de mensagem novamente

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-   return  <h1>Mensagem de aviso de que eu, Michele, estou aprendendo deploy na Plataforma Vercel.</h1>
+   return  <h1>Mensagem de aviso de que eu, Michele, estou aprendendo deploy na Plataforma Vercel. Mensagem 2! </h1>
 }
 export default Home;
 
