141e70bd2010e2873355e6622d266a2e1b533542
Mi Seadi <100794047+michele-seadi@users.noreply.github.com>
2024-05-17 17:07:35 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-function Home(){
-   return  <h1>Mensagem de aviso de que eu, Michele, estou aprendendo deploy na Plataforma Vercel. Mensagem 2! </h1>
+function Home() {
+  return (
+    <h1>
+      Mensagem de aviso de que eu, Michele, estou aprendendo deploy na
+      Plataforma Vercel. Mensagem 2!{" "}
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("identação errada");
 }
 export default Home;
-
