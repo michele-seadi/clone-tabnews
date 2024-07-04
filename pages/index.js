22176d56c57530be70e10b5c4240f2cfc060b626
Mi Seadi <100794047+michele-seadi@users.noreply.github.com>
2024-07-04 17:44:08 +0000

    altera index.js de 2 para 3

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home() {
-  return (
-    <h1>
-     Plataforma de testes: Aprendizagem WEB Mensagem 2!{" "}
-    </h1>
-  );
+  return <h1>Plataforma de testes: Aprendizagem WEB Mensagem 3! </h1>;
 }
 
 export default Home;
