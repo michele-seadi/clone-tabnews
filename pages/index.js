dda3d4833c6791a7c16a86c4029e93526fa6df41
Mi Seadi <100794047+michele-seadi@users.noreply.github.com>
2024-07-04 18:16:21 +0000

    altera codigo indes.js para aparecer hora

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+
 function Home() {
-  return <h1>Plataforma de testes: Aprendizagem WEB Mensagem 5! </h1>;
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    function updateClock() {
+      let d = new Date();
+      setTime(`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`);
+    }
+
+    const interval = setInterval(updateClock, 1000);
+
+    // Chama a função uma vez para definir o tempo imediatamente
+    updateClock();
+
+    // Limpa o intervalo quando o componente é desmontado
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <h1 id="demo">Plataforma de testes: Aprendizagem WEB Mensagem 5! {time}</h1>
+  );
 }
 
 export default Home;
