7824ec1236d1b832847ba13b4b341d9dfe4196b2
Mi Seadi <100794047+michele-seadi@users.noreply.github.com>
2024-07-04 17:49:53 +0000

    altera msg inex.js de 3 para 4

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Plataforma de testes: Aprendizagem WEB Mensagem 3! </h1>;
+  return <h1>Plataforma de testes: Aprendizagem WEB Mensagem 4! </h1>;
 }
 
 export default Home;
